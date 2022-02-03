a101652a052360abc6eed64e227d5ebf0d109b6a
Broström.A | Evul <andreas.brostrom.ce@gmail.com>
2022-02-03 16:07:28 +0100

    Update deploy.sh

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+VERSION=$*
+PROJECT_NAME=""
+PROJECT_FILES=common
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT_DIR=$SCRIPT_DIR
+
+mkdir $PROJECT_ROOT_DIR/release/
+
+python3 $SCRIPT_DIR/make.py
+
+cd $PROJECT_ROOT_DIR
+
+
+
+for I in $OPROJECT_FILES
+do
+  echo "Copying $I for release"
+  cp -R "$I" "$PROJECT_ROOT_DIR/release/."
+done
