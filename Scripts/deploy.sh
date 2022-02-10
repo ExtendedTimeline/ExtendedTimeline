8b200efb716f2eebc554e5442cc692357bcc09c6
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-02-10 17:19:13 +0100

    Script update

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+set -e
 
 VERSION=$*
-PROJECT_NAME="NextExtended"
-PROJECT_FILES=common
+PROJECT_NAME="ExtendedTimeline" 
+PROJECT_FILES=("common" "events" "gfx" "interface" "localisation" "ExtendedTimeline.mod" "LICENSE" "README.md")
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROJECT_ROOT_DIR="$SCRIPT_DIR/../"
-
-mkdir $PROJECT_ROOT_DIR/release/
-
-python3 $SCRIPT_DIR/make.py
+PROJECT_ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 cd $PROJECT_ROOT_DIR
 
+mkdir -p $PROJECT_ROOT_DIR/release/$PROJECT_NAME
 
+#python3 $SCRIPT_DIR/make.py
 
-for I in $OPROJECT_FILES
+for I in ${PROJECT_FILES[@]}
 do
   echo "Copying $I for release"
-  cp -R "$I" "$PROJECT_ROOT_DIR/release/."
+  cp -Rp "$I" "$PROJECT_ROOT_DIR/release/$PROJECT_NAME/."
 done
+
+sed -i "s/0.0.0/${VERSION}/" $PROJECT_ROOT_DIR/release/$PROJECT_NAME/ExtendedTimeline.mod
